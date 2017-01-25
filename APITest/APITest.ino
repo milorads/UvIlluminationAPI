d40ebf1947e345cb32fd1b71029f4bf246672ddb
msekulovic <milorad.sekulovic@gmail.com>
2017-01-25 15:28:04 +0100

    added UV intensity part

--- APITest/APITest.ino
+++ APITest/APITest.ino
@@ -9,6 +9,8 @@ int REF_3V3 = A1; //3.3V power on the Arduino board
 int BH1750_address = 0x23; // i2c Address for Illumination sensor
 byte buff[2];
 
+
+
 // Listen on default port 5555, the webserver on the Yun
 // will forward there all the HTTP requests for us.
 YunServer server;
@@ -61,6 +63,19 @@ void process(YunClient client) {
 }
 
 void uvCommand(YunClient client) {
+  int uvLevel = averageAnalogRead(UVOUT);
+  int refLevel = averageAnalogRead(REF_3V3);
+
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
+
+  client.print("UV Intensity (mW/cm^2): ");
+  client.println(uvIntensity);
+}
+
+void ledCommand(YunClient client) {
   
 }
 
@@ -76,10 +91,6 @@ void illuminationCommand(YunClient client) {
     
     client.println(" lx"); 
   }
-}
-
-void ledCommand(YunClient client) {
-  
 }
 
 void BH1750_Init(int address){
@@ -100,4 +111,25 @@ byte BH1750_Read(int address){
   }
   Wire.endTransmission();  
   return i;
+}
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
