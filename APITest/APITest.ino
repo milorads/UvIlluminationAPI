a3d83a81bb896d886248f0624b86499927af58a0
msekulovic <milorad.sekulovic@gmail.com>
2017-01-25 15:19:58 +0100

    wrote function to get the illumination value

--- APITest/APITest.ino
+++ APITest/APITest.ino
@@ -6,6 +6,9 @@
 int UVOUT = A0; //Output from the sensor
 int REF_3V3 = A1; //3.3V power on the Arduino board
 
+int BH1750_address = 0x23; // i2c Address for Illumination sensor
+byte buff[2];
+
 // Listen on default port 5555, the webserver on the Yun
 // will forward there all the HTTP requests for us.
 YunServer server;
@@ -13,6 +16,8 @@ YunServer server;
 void setup() {
   // Bridge startup
   Bridge.begin();
+  Wire.begin();
+  BH1750_Init(BH1750_address);
   
   pinMode(UVOUT, INPUT);
   pinMode(REF_3V3, INPUT);
@@ -42,123 +47,57 @@ void process(YunClient client) {
   // read the command
   String command = client.readStringUntil('/');
 
-  // is "digital" command?
   if (command == "uv") {
     uvCommand(client);
   }
 
-  // is "analog" command?
   if (command == "illumination") {
     illuminationCommand(client);
   }
 
-  // is "mode" command?
   if (command == "led") {
     ledCommand(client);
   }
 }
 
 void uvCommand(YunClient client) {
-  int pin, value;
-
-  // Read pin number
-  pin = client.parseInt();
-
-  // If the next character is a '/' it means we have an URL
-  // with a value like: "/digital/13/1"
-  if (client.read() == '/') {
-    value = client.parseInt();
-    digitalWrite(pin, value);
-  }
-  else {
-    value = digitalRead(pin);
-  }
-
-  // Send feedback to client
-  client.print(F("Pin D"));
-  client.print(pin);
-  client.print(F(" set to "));
-  client.println(value);
-
-  // Update datastore key with the current pin value
-  String key = "D";
-  key += pin;
-  Bridge.put(key, String(value));
+  
 }
 
-void analogCommand(YunClient client) {
-  int pin, value;
+void illuminationCommand(YunClient client) {
+    float valf=0;
 
-  // Read pin number
-  pin = client.parseInt();
-
-  // If the next character is a '/' it means we have an URL
-  // with a value like: "/analog/5/120"
-  if (client.read() == '/') {
-    // Read value and execute command
-    value = client.parseInt();
-    analogWrite(pin, value);
-
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" set to analog "));
-    client.println(value);
-
-    // Update datastore key with the current pin value
-    String key = "D";
-    key += pin;
-    Bridge.put(key, String(value));
-  }
-  else {
-    // Read analog pin
-    value = analogRead(pin);
-
-    // Send feedback to client
-    client.print(F("Pin A"));
-    client.print(pin);
-    client.print(F(" reads analog "));
-    client.println(value);
-
-    // Update datastore key with the current pin value
-    String key = "A";
-    key += pin;
-    Bridge.put(key, String(value));
+  if(BH1750_Read(BH1750_address)==2){
+    
+    valf=((buff[0]<<8)|buff[1])/1.2;
+    
+    if(valf<0)client.print("> 65535");
+    else client.print((int)valf,DEC); 
+    
+    client.println(" lx"); 
   }
 }
 
-void modeCommand(YunClient client) {
-  int pin;
+void ledCommand(YunClient client) {
+  
+}
 
-  // Read pin number
-  pin = client.parseInt();
+void BH1750_Init(int address){
+  
+  Wire.beginTransmission(address);
+  Wire.write(0x10);
+  Wire.endTransmission();
+}
 
-  // If the next character is not a '/' we have a malformed URL
-  if (client.read() != '/') {
-    client.println(F("error"));
-    return;
+byte BH1750_Read(int address){
+  
+  byte i=0;
+  Wire.beginTransmission(address);
+  Wire.requestFrom(address, 2);
+  while(Wire.available()){
+    buff[i] = Wire.read(); 
+    i++;
   }
-
-  String mode = client.readStringUntil('\r');
-
-  if (mode == "input") {
-    pinMode(pin, INPUT);
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" configured as INPUT!"));
-    return;
-  }
-
-  if (mode == "output") {
-    pinMode(pin, OUTPUT);
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" configured as OUTPUT!"));
-    return;
-  }
-
-  client.print(F("error: invalid mode "));
-  client.print(mode);
+  Wire.endTransmission();  
+  return i;
 }
